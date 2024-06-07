b703c90d4ea24a9680a2a18f6727e1d41060a3a3
zmathantala <zmathantala@e4.co.za>
2024-06-07 02:36:26 +0200

    first commit

---
+++ Base.cs
@@ -0,0 +1,70 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSelenium
+{
+    public class Base 
+    {
+        static IWebDriver driver = new ChromeDriver();
+
+
+        
+        public static void Main(string[] args)
+        {
+
+            //institiating the driver
+            // maximase and open the browser
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.demo.guru99.com/test/newtours/");
+           
+            
+
+
+            /*Thread.Sleep(2000);
+            //driver.FindElement(By.LinkText("REGISTER")).Click();
+            String PageTitle= driver.Title;
+            Console.WriteLine(PageTitle);
+            String expectedTitle = "Welcome: Mercury Tours";
+
+            if (PageTitle.Contains(expectedTitle))
+            {
+                Console.WriteLine("Title is matching with expected value");
+            }
+            else
+            {
+                Console.WriteLine("Title is not matching with expected value");
+            }
+
+
+            */
+
+            driver.Quit();
+
+        }//end mian
+        public void Register()
+        {
+            driver.FindElement(By.XPath("//input[@name='firstName']")).SendKeys("Zizipho");
+            driver.FindElement(By.XPath("//input[@name='lastName']")).SendKeys("Zizipho");
+            driver.FindElement(By.XPath("//input[@name='phone']")).SendKeys("0672995643");
+            driver.FindElement(By.XPath("//input[@id='userName']")).SendKeys("Zizipho105!");
+
+            driver.FindElement(By.XPath("//input[@name='address1']")).SendKeys("chesterville");
+            driver.FindElement(By.XPath("//input[@name='city']")).SendKeys("Durban");
+            driver.FindElement(By.XPath("//input[@name='state']")).SendKeys("KZN");
+            driver.FindElement(By.XPath("//input[@name='postalCode']")).SendKeys("2345");
+
+            // Create object of the Select class
+            Select se = new Select(driver.findElement(By.xpath("//*[@id='oldSelectMenu']")));
+
+            // Select the option by index
+            se.selectByIndex(3);
+
+            driver.FindElement(By.LinkText("REGISTER")).Click();
+        }
+    }//end class
+}
---
+++ Register.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSelenium
+{
+    internal class Register
+    {
+
+        private IWebDriver driver;
+
+        public Register()
+        {
+        }
+
+        public Register(IWebDriver driver)
+        {
+            this.driver = driver;
+            //PageFactory.InitElements(driver, this);
+        }
+        public static void RegisterUser(WebDriver driver) 
+        {
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("REGISTER")).Click();
+
+        }
+    }
+}
